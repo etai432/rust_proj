4f173c48f84fa85088bfb6d912d0250192e5edb4
etai432 <etaizilberman15@gmail.com>
2022-10-18 02:10:48 +0300

    hi

---
+++ target/debug/deps/rust_proj-479313062cc0dbfa.d
@@ -0,0 +1,5 @@
+c:\Users\User\rust_proj\target\debug\deps\rust_proj-479313062cc0dbfa.rmeta: src\main.rs
+
+c:\Users\User\rust_proj\target\debug\deps\rust_proj-479313062cc0dbfa.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/rust_proj-ba05b033cf0c674c.d
@@ -0,0 +1,5 @@
+c:\Users\User\rust_proj\target\debug\deps\rust_proj-ba05b033cf0c674c.rmeta: src\main.rs
+
+c:\Users\User\rust_proj\target\debug\deps\rust_proj-ba05b033cf0c674c.d: src\main.rs
+
+src\main.rs:
