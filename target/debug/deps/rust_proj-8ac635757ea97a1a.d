718cbc266d3125c77a6e7712f783efbc402cf0cc
etai432 <etaizilberman15@gmail.com>
2022-10-18 02:07:54 +0300

    4th commit

---
+++ target/debug/deps/rust_proj-8ac635757ea97a1a.d
@@ -0,0 +1,5 @@
+/home/itay/Documents/GitHub/rust_proj/target/debug/deps/rust_proj-8ac635757ea97a1a.rmeta: src/main.rs
+
+/home/itay/Documents/GitHub/rust_proj/target/debug/deps/rust_proj-8ac635757ea97a1a.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rust_proj-9ffed4fc76fca618.d
@@ -0,0 +1,5 @@
+/home/itay/Documents/GitHub/rust_proj/target/debug/deps/rust_proj-9ffed4fc76fca618.rmeta: src/main.rs
+
+/home/itay/Documents/GitHub/rust_proj/target/debug/deps/rust_proj-9ffed4fc76fca618.d: src/main.rs
+
+src/main.rs:
