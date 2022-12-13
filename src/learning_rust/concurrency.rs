e940cd6844b2ed3c35577e593817b21dd4b8fe68
etai432 <etaizilberman15@gmail.com>
2022-12-13 02:40:20 +0200

    ok

--- src/learning_rust/concurrency.rs
+++ src/learning_rust/concurrency.rs
@@ -1,23 +1,34 @@
+use std::cmp::Reverse;
 use std::thread;
-use std::time::Duration;
+// use std::time::Duration;
+use std::sync::mpsc;
 
 pub fn run() {
-    let v = vec![1, 2, 3];
+    // let v = vec![1, 2, 3];
 
-    let handle = thread::spawn(move || {
-        for i in 1..10 {
-            println!("spawned: {}", i);
-            thread::sleep(Duration::from_millis(1));
-        }
-        println!("vector: {:?}", v)
-    });
+    // let handle = thread::spawn(move || {
+    //     for i in 1..10 {
+    //         println!("spawned: {}", i);
+    //         thread::sleep(Duration::from_millis(1));
+    //     }
+    //     println!("vector: {:?}", v)
+    // });
 
-    //cannot use v after it was moved
+    // //cannot use v after it was moved
 
-    for i in 1..5 {
-        println!("main: {}", i);
-        thread::sleep(Duration::from_millis(1));
-    }
+    // for i in 1..5 {
+    //     println!("main: {}", i);
+    //     thread::sleep(Duration::from_millis(1));
+    // }
 
-    handle.join().unwrap(); // waits for the handle thread to finish
+    // handle.join().unwrap(); // waits for the handle thread to finish
+
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let msg = String::from("hi");
+        tx.send(msg).unwrap();
+    }); 
+
+    let received = rx.recv().unwrap();
+    println!("msg: {}", received)
 }
