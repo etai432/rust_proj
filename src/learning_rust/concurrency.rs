01cb73fb2d1fbcf3b4594c62bc764630255f094e
etai432 <etaizilberman15@gmail.com>
2022-12-14 01:57:12 +0200

    ok

--- src/learning_rust/concurrency.rs
+++ src/learning_rust/concurrency.rs
@@ -1,6 +1,7 @@
 use std::thread;
-use std::time::Duration;
-use std::sync::mpsc;
+// use std::time::Duration;
+// use std::sync::mpsc;
+use std::sync::{Mutex, Arc};
 
 pub fn run() {
     // let v = vec![1, 2, 3];
@@ -22,25 +23,44 @@ pub fn run() {
 
     // handle.join().unwrap(); // waits for the handle thread to finish
 
-    let (tx, rx) = mpsc::channel();
-    let tx2 = tx.clone();
-    thread::spawn(move || {
-        let vals = vec!["hi", "hello", "test"];
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
+    // let (tx, rx) = mpsc::channel();
+    // let tx2 = tx.clone();
+    // thread::spawn(move || {
+    //     let vals = vec!["hi", "hello", "test"];
+    //     for val in vals {
+    //         tx.send(val).unwrap();
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // });
     
-    thread::spawn(move || {
-        let vals = vec!["hi1", "hello1", "test1"];
-        for val in vals {
-            tx2.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
+    // thread::spawn(move || {
+    //     let vals = vec!["hi1", "hello1", "test1"];
+    //     for val in vals {
+    //         tx2.send(val).unwrap();
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // });
     
-    for received in rx {
-        println!("msg: {}", received)
+    // for received in rx {
+    //     println!("msg: {}", received)
+    // }
+
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+
+        handles.push(handle);
     }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("{}", *counter.lock().unwrap());
 }
