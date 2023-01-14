72916720bafc8a80823a607b14859afc0193c43f
etai432 <etaizilberman15@gmail.com>
2023-01-14 19:09:04 +0200

    ok

--- src/file_renamer.rs
+++ src/file_renamer.rs
@@ -12,9 +12,8 @@ pub fn run() {
         let in_vec: Vec<&str> = input.split(" ").collect();
         if in_vec.len() > 0 {
             match in_vec[0] {
-                "cmd" => println!("commands: ls, lsc, cd, root, env, this, current, back, cdp, cdr, rename"),
-                "ls" => for file in fs::read_dir(&current).unwrap(){println!("{}", file.unwrap().path().display());},
-                "lsc" => if in_vec.len() > 1{for file in fs::read_dir(&current).unwrap(){if String::from(file.as_ref().unwrap().path().to_str().unwrap()).contains(in_vec[1]){println!("{}", file.unwrap().path().display());}}}else{println!("not given an argument")},
+                "cmd" => println!("commands: ls, cd, root, env, this, current, back, cdp, cdr, rename"),
+                "ls" => if in_vec.len() > 1{for file in fs::read_dir(&current).unwrap(){if String::from(file.as_ref().unwrap().path().to_str().unwrap()).contains(in_vec[1]){println!("{}", file.unwrap().path().display());}}}else{for file in fs::read_dir(&current).unwrap(){println!("{}", file.unwrap().path().display());}},
                 "cd" => if in_vec.len() > 1{for file in fs::read_dir(&current).unwrap(){if file.as_ref().unwrap().path().to_str().unwrap().eq(&(current.clone() + in_vec[1..in_vec.len()].join(" ").as_str())){current+=in_vec[1..in_vec.len()].join(" ").as_str();current+=r"\";break;}}}else{println!("not given an argument")},
                 "root" => current = String::from(root.clone()),
                 "env" => current = String::from(abs_path.to_str().unwrap()) + r"\",
