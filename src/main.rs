e1f215d4e330868149f4808253826bf90eb44932
etai432 <etaizilberman15@gmail.com>
2023-01-14 17:53:57 +0200

    ok

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@
 // use game_of_life::game;
 // mod chess;
 // use chess::chess_pvp::chess_pvp;
-mod renamer;
+mod file_renamer;
 
 fn main() {
     // chess_pvp();
-    renamer::run()
+    file_renamer::run()
 }
