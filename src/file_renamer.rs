afe53d31cde61df48ff0be0ce95e6f9ee0ac3eac
etai432 <etaizilberman15@gmail.com>
2023-01-15 10:10:52 +0200

    ok

--- src/file_renamer.rs
+++ src/file_renamer.rs
@@ -2,7 +2,7 @@ use std::{io, fs, env, file};
 
 pub fn run() {
     let abs_path = env::current_dir().unwrap();
-    let mut root = String::from(abs_path.to_str().unwrap().split(r"\").collect::<Vec<&str>>()[0]);
+    let mut root = String::from(abs_path.to_str().unwrap().replace(r"/", r"\").split(r"\").collect::<Vec<&str>>()[0]).replace(r"/", r"\");
     root.push_str(r"\");
     let mut current = String::from(root.clone());
     let mut input = String::new();
@@ -16,8 +16,8 @@ pub fn run() {
                 "ls" => if in_vec.len() > 1{for file in fs::read_dir(&current).unwrap(){if String::from(file.as_ref().unwrap().path().to_str().unwrap()).contains(in_vec[1]){println!("{}", file.unwrap().path().display());}}}else{for file in fs::read_dir(&current).unwrap(){println!("{}", file.unwrap().path().display());}},
                 "cd" => if in_vec.len() > 1{for file in fs::read_dir(&current).unwrap(){if file.as_ref().unwrap().path().to_str().unwrap().eq(&(current.clone() + in_vec[1..in_vec.len()].join(" ").as_str())){current+=in_vec[1..in_vec.len()].join(" ").as_str();current+=r"\";break;}}}else{println!("not given an argument")},
                 "root" => current = String::from(root.clone()),
-                "env" => current = String::from(abs_path.to_str().unwrap()) + r"\",
-                "this" => current = String::from(abs_path.to_str().unwrap()) + r"\" + file!() + r"\",
+                "env" => current = (String::from(abs_path.to_str().unwrap()) + r"\").replace(r"/", r"\"),
+                "this" => current = (String::from(abs_path.to_str().unwrap()) + r"\" + file!() + r"\").replace(r"/", r"\"),
                 "current" => println!("{}", current),
                 "back" => current = current.split(r"\").collect::<Vec<&str>>().split_last().unwrap().1.split_last().unwrap().1.join(r"\") + r"\",
                 "cdp" => if in_vec.len() > 1 && fs::metadata(String::from(in_vec[1])).is_ok(){current=String::from(in_vec[1])+r"\"}else{println!("couldnt find path")}
