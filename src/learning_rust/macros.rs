6c1444e0fd478ca39883e571c2138664f18db361
etai432 <etaizilberman15@gmail.com>
2023-01-24 00:55:05 +0200

    ok

--- src/learning_rust/macros.rs
+++ src/learning_rust/macros.rs
@@ -1,6 +1,10 @@
+use std::any::Any;
+
+use paste::paste;
+
 #[macro_use]
 macro_rules! items {
-    ( $($x:expr),*) => {
+    ( $($x:expr),* ) => {
         let mut num = 1;
         $(
             println!("{} is item number {}", $x, num);
@@ -9,6 +13,49 @@ macro_rules! items {
     };
 }
 
+macro_rules! create_struct {
+    ($struct_name:ident, $(($field_name:ident: $field_type:ty)), *) => {
+        #[derive(Default)]
+        struct $struct_name {
+            $(
+                $field_name: $field_type,
+            )*
+        }
+        impl $struct_name {
+            fn new($($field_name: $field_type), *) -> $struct_name {
+                $struct_name {
+                    $(
+                        $field_name: $field_name,
+                    )*
+                }
+            }
+
+            paste! {
+                $(
+                    fn [<get_$field_name>](&self) -> &$field_type {
+                        &self.$field_name
+                    }
+
+                    fn [<set_$field_name>](&mut self, $field_name:$field_type) {
+                        self.$field_name = $field_name;
+                    }
+                )*
+                fn to_string(&self) -> String {
+                    let mut string = format!("{} = [", stringify!($struct_name));
+                    $(
+                        string += format!("({} = {}), ",stringify!($field_name), self.[<get_$field_name>]()).as_str();
+                    )*
+                    string[..string.len()-2].to_string() + "]"
+                }
+            }
+        }
+    };
+}
+
 pub fn run() {
-    items![2, 5, 7, "hello"];
+    // items![2, 5, 7, "hello"];
+    create_struct!(stru, (field1: i32), (field2: String), (field3: f64));
+    let mut stru:stru = Default::default();
+    stru.set_field2("hello".to_string());
+    println!("{}", stru.field1)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 // mod game_of_life;
 // use game_of_life::game;
-mod chess;
-use chess::chess_pvp::chess_pvp;
+// mod chess;
+// use chess::chess_pvp::chess_pvp;
 // mod file_renamer;
-// mod learning_rust;
-// use learning_rust::macros;
+mod learning_rust;
+use learning_rust::macros;
 
 fn main() {
-    chess_pvp();
+    // chess_pvp();
     // file_renamer::run()
-    // macros::run();
+    macros::run();
 }
