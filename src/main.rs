a6f93becf5fa281879967ad226f849ca8a93859f
etai432 <etaizilberman15@gmail.com>
2023-01-25 14:47:10 +0200

    ok

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@
 // mod chess;
 // use chess::chess_pvp::chess_pvp;
 // mod file_renamer;
-mod learning_rust;
-use learning_rust::macros;
+// mod learning_rust;
+// use learning_rust::macros;
+mod thread_pool;
 
 fn main() {
     // chess_pvp();
     // file_renamer::run()
-    macros::run();
+    // macros::run();
+    thread_pool::build_vec();
 }
