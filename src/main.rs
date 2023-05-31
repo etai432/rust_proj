b6491a473395794176f1d7a501f69c07c5695b01
etai432 <etaizilberman@gmail.com>
2023-05-31 22:48:53 +0300

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 // mod chess;
 // mod learning_rust;
+// mod export_test;
 mod physics_engine;
 
 fn main() {
-    physics_engine::graphics::run();
+    // physics_engine::graphics::run();
     // chess::chess_pvp::chess_pvp();
     // learning_rust::iterators::run();
 }
